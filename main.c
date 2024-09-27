9b096609c1e815bf6ba9f4108bc7fa4ece605613
Anna Julia Curcio Marques <134973253+annaju0405@users.noreply.github.com>
2024-09-27 01:26:15 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "forca.h"
+
+int main() {
+    abertura();
+    escolhepalavra();
+
+    do {
+        desenhaforca();
+        chuta();
+    } while (!ganhou() && !enforcou());
+
+    if(ganhou()) {
+        printf("\nParabéns, você ganhou!\n\n");
+        printf("       ___________      \n");
+        printf("      '._==_==_=_.'     \n");
+        printf("      .-\\:      /-.    \n");
+        printf("     | (|:.     |) |    \n");
+        printf("      '-|:.     |-'     \n");
+        printf("        \\::.    /      \n");
+        printf("         '::. .'        \n");
+        printf("           ) (          \n");
+        printf("         _.' '._        \n");
+        printf("        '-------'       \n\n");
+    } else {
+        printf("\nPuxa, você foi enforcado!\n");
+        printf("A palavra era **%s**\n\n", palavrasecreta);
+        printf("    _______________         \n");
+        printf("   /               \\       \n"); 
+        printf("  /                 \\      \n");
+        printf("//                   \\/\\  \n");
+        printf("\\|   XXXX     XXXX   | /   \n");
+        printf(" |   XXXX     XXXX   |/     \n");
+        printf(" |   XXX       XXX   |      \n");
+        printf(" |                   |      \n");
+        printf(" \\__      XXX      __/     \n");
+        printf("   |\\     XXX     /|       \n");
+        printf("   | |           | |        \n");
+        printf("   | I I I I I I I |        \n");
+        printf("   |  I I I I I I  |        \n");
+        printf("   \\_             _/       \n");
+        printf("     \\_         _/         \n");
+        printf("       \\_______/           \n");
+    }
+
+    adicionapalavra();
+    return 0;
+}
